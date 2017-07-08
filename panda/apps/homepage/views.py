2f7c15c73473375ce04845e91f19702545a31f20
Panda_annie <Panda_annie@gmail.com>
2017-07-08 21:45:46 +0800

    optimize "get in touch" implementation

--- panda/apps/homepage/views.py
+++ panda/apps/homepage/views.py
@@ -1,29 +1,22 @@
-
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, FormView
 from django.utils.translation import ugettext_lazy as _
-from .forms import ContactForm
-from django.shortcuts import render
 
+from .forms import ContactForm
 from django.core.mail import send_mail
 from django.template.loader import get_template
 from django.template import Context
+from django.conf import settings
 
-class HomePageView(TemplateView):
+
+class HomePageView(FormView):
     template_name = 'homepage/home.html'
     page_title = _('Home Page')
     active_tab = 'Home Page'
+    form_class = ContactForm
+    success_url = reverse_lazy('message_sent')
 
-
-class PromotionPageView(TemplateView):
-    template_name = 'homepage/promotion.html'
-    page_title = _('Promotion Page')
-    active_tab = 'Promotion Page'
-
-
-def msg_sent(request):
-    form = ContactForm(request.POST)
-
-    if form.is_valid():
+    def form_valid(self, form):
         name = form.cleaned_data['name']
         place = form.cleaned_data['place']
         email = form.cleaned_data['email']
@@ -37,10 +30,22 @@ def msg_sent(request):
             'email': email,
             'message': message
         }))
-        print("Sending email" + msg)
 
-        recipients = ['pandaannielai@gmail.com']
+        recipients = [settings.EMAIL_HOST_USER]
         send_mail('You got a message from user',
-                  msg, email,
+                  msg, settings.EMAIL_HOST_USER,
                   recipients)
-        return render(request, 'homepage/msg_sent.html', {'form': form})
+        return super(HomePageView, self).form_valid(form)
+
+
+class PromotionPageView(TemplateView):
+    template_name = 'homepage/promotion.html'
+    page_title = _('Promotion Page')
+    active_tab = 'Promotion Page'
+
+
+class MsgSendView(TemplateView):
+    template_name = 'homepage/msg_sent.html'
+    page_title = _('Thanks Page')
+    active_tab = 'Thanks Page'
+
