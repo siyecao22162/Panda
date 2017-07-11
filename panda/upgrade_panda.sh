57cb1d68669114ceee4aed5528e87207b60fd0c7
Panda_annie <Panda_annie@gmail.com>
2017-07-11 23:16:37 +0800

    backup upgrade panda

---
+++ panda/upgrade_panda.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+cd ~/Panda
+git pull
+cd
+mv webapps/django/panda ./panda.`date +%Y%m%d%H%M%S`
+cp -rf Panda webapps/django/panda
+cd webapps/django/panda/
+. ../env/bin/activate
+make panda
+
+/home/nkannielai/webapps/django/apache2/bin/stop
+sleep 2
+/home/nkannielai/webapps/django/apache2/bin/start
+
