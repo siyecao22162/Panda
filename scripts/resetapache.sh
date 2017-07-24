ce0e9b8cf617a727ab703655e6386833d6900440
server <registeronly@126.com>
2017-07-24 13:56:55 +0000

    Revert "#!/bin/bash"
    This reverts commit 67f03e08a4e09102e96a108e5a828f052b17c51e.

--- scripts/resetapache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/home/nkannielai/webapps/django/apache2/bin/stop
-sleep 2
-/home/nkannielai/webapps/django/apache2/bin/start
-
--- scripts/start_apache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-source ~/.bash_profile
-env >~/webapps/django/env.txt
-~/webapps/django/apache2/bin/start
-
-
