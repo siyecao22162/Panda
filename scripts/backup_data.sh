8fb135753d48ba46351ea40d073dd9098e05d279
server <registeronly@126.com>
2017-07-26 14:35:00 +0000

    Fix the error of dbbackup binary

--- scripts/backup_data.sh
+++ scripts/backup_data.sh
@@ -3,8 +3,13 @@
 set -x
 source /home/nkannielai/.bash_profile
 . /home/nkannielai/webapps/django/env/bin/activate
-/home/nkannielai/webapps/django/panda/panda/manage.py dbbackup
-/home/nkannielai/webapps/django/panda/panda/manage.py mediabackup
+
+# For dbbackup's binary copy mode, need to cd to db.sqlite path
+cd /home/nkannielai/webapps/django/panda/panda/
+
+# Actual ./manage.py is also ok....
+/home/nkannielai/webapps/django/panda/panda/manage.py dbbackup -s wb
+/home/nkannielai/webapps/django/panda/panda/manage.py mediabackup -s wb
 
 date >> /home/nkannielai/webapps/django/backup_time.log
 
--- scripts/upgrade_panda.sh
+++ scripts/upgrade_panda.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# set echo
 set -x
+
+# pull from git
 cd ~/Panda
 git reset --hard
 git pull
+
+# backup old files and put new files
 cd
+cp webapps/django/panda/panda/db.sqlite .
 mv webapps/django/panda backups/panda.`date +%Y%m%d%H%M%S`
 cp -rf Panda webapps/django/panda
+cp db.sqlite webapps/django/panda/panda/
+
+# start make
 cd webapps/django/panda/
 . ../env/bin/activate
 make install
@@ -14,6 +23,7 @@ panda/manage.py makemigrations
 panda/manage.py migrate
 panda/manage.py collectstatic --noinput
 
+# restart apache2
 /home/nkannielai/webapps/django/apache2/bin/stop
 sleep 2
 /home/nkannielai/webapps/django/apache2/bin/start
