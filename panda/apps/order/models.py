a87e453748e48a568c3de771ae80aafca3be28da
annie <annielai88@yahoo.com>
2017-11-08 22:12:38 +0100

    Remove gemalto from default address

--- panda/apps/order/models.py
+++ panda/apps/order/models.py
@@ -4,9 +4,8 @@ class ShippingAddress(AbstractShippingAddress):
     # don't know why direct overwrite phone_number is not working. Just for django abstract class?
     AbstractShippingAddress._meta.get_field('phone_number').blank = False
 
-    # Add "GEMALTO 13600 LA CIOTAT" as default address to suit PRE-OPENING cases. TODO: maybe all France later?
+    # Add "GEMALTO 13600 LA CIOTAT" as default address to suit PRE-OPENING cases.
     AbstractShippingAddress._meta.get_field('postcode').default = "13600"
-    AbstractShippingAddress._meta.get_field('line1').default = "GEMALTO"
     AbstractShippingAddress._meta.get_field('line4').default = "LA CIOTAT"
 
 
